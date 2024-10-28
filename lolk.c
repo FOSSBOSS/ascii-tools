2ed7f4590fd9277a0ab1313533b82f29f2b8e7e3
FOSSBOSS <blicken@gmail.com>
2024-10-27 21:47:43 -0400

    Update lolk.c
    added arg for less colour changing

--- lolk.c
+++ lolk.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-// gcc lolk.c -o lolk
-//
-// its not a direct clone of lolcat and it works a little differently.
-// You can pipe into it though, and it doesnt conflict with figlet in the same way.
-// Hey, lets see if it conflicts with fileRoll, and dialog. 
-// the buffer size could be an issue....
+#include <stdlib.h>
+// now with -n arg for number of characters to skipp before changing colour
 const char *colors[] = {
     "\033[31m",  // Red
     "\033[33m",  // Yellow
@@ -19,19 +15,41 @@ const char *reset = "\033[0m";
 
 int main(int argc, char *argv[]) {
     int color_index = 0;
+    int shift_interval = 1;  // Default to 1 character before color change
+    char *input_text = NULL;  // Pointer for text input if provided as argument
 
-    if (argc > 1) {
-        // Process the argument text
-        for (int i = 0; i < strlen(argv[1]); i++) {
-            printf("%s%c", colors[color_index % 6], argv[1][i]);
-            color_index++;
+    // Parse arguments for optional -n flag and text input
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-n") == 0 && i + 1 < argc) {
+            shift_interval = atoi(argv[i + 1]);
+            i++;  // Skip the next argument as it's part of -n
+        } else {
+            input_text = argv[i];  // Treat any remaining argument as text input
+        }
+    }
+
+    int char_count = 0;
+
+    if (input_text) {
+        // Process the argument text if provided
+        for (int i = 0; i < strlen(input_text); i++) {
+            printf("%s%c", colors[color_index % 6], input_text[i]);
+            char_count++;
+            if (char_count == shift_interval) {
+                color_index++;
+                char_count = 0;
+            }
         }
     } else {
-        // Process stdin input
+        // Process stdin input if no text argument is provided
         char ch;
         while ((ch = getchar()) != EOF) {
             printf("%s%c", colors[color_index % 6], ch);
-            color_index++;
+            char_count++;
+            if (char_count == shift_interval) {
+                color_index++;
+                char_count = 0;
+            }
         }
     }
 
